8e65bf5d10672617dd19a2bc58674bbd5228f7ba
Victor Jose Corral <victorjcorral@icloud.com>
2025-07-25 18:54:25 -0700

    Update to v0.1.2 with corrected build and SHA

--- Formula/ritual.rb
+++ Formula/ritual.rb
@@ -1,18 +1,16 @@
 class Ritual < Formula
   include Language::Python::Virtualenv
 
-  desc "Portable CLI tool for cryptographic signing with PGPy"
+  desc "Cryptographic signing and verification CLI using PGPy"
   homepage "https://github.com/GeneXisD/ritual"
-  url "https://github.com/GeneXisD/ritual/archive/refs/tags/v0.1.1.tar.gz"
+  url "https://github.com/GeneXisD/ritual/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "3ee4c63164789dc35d389293e30b2bb0d2c236f5d910fb8eb909f58d5114a450"
-  version "0.1.1"
-  license "GPL-3.0-or-later"
+  license "MIT"
 
   depends_on "python@3.12"
 
   def install
     virtualenv_install_with_resources
-    bin.install "ritual.py" => "ritual"
   end
 
   test do
