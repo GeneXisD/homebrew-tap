b5b48cf298c490e76a4ba1449bcad8b39f47786d
Victor J Corral <victorjcorral@icloud.com>
2025-07-25 18:20:53 -0700

    Update ritual.rb

--- Formula/ritual.rb
+++ Formula/ritual.rb
@@ -1,13 +1,17 @@
 class Ritual < Formula
-  desc "Cryptographic signing tool for SPDX, RFCs, and RitualMesh"
+  include Language::Python::Virtualenv
+
+  desc "Portable CLI tool for cryptographic signing with PGPy"
   homepage "https://github.com/GeneXisD/ritual"
-  url "https://github.com/GeneXisD/ritual/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "055f46077de82eb4a40346ccb8057769f3efb8673a9fa3bc28e4aefe67d82aae"
-  license "MIT"
+  url "https://github.com/GeneXisD/ritual/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "3ee4c63164789dc35d389293e30b2bb0d2c236f5d910fb8eb909f58d5114a450"
+  version "0.1.1"
+  license "GPL-3.0-or-later"
 
   depends_on "python@3.12"
 
   def install
+    virtualenv_install_with_resources
     bin.install "ritual.py" => "ritual"
   end
 
@@ -15,4 +19,7 @@ class Ritual < Formula
     system "#{bin}/ritual", "--help"
   end
 end
+
+
+
 
