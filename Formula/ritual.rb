15e9967ae5d2ea502108cc4bb8868187efb5d724
Victor Jose Corral <victorjcorral@icloud.com>
2025-07-25 19:20:33 -0700

    Fix entry point to ritual_cli:main

--- Formula/ritual.rb
+++ Formula/ritual.rb
@@ -4,7 +4,7 @@ class Ritual < Formula
   desc "Cryptographic signing and verification CLI using PGPy"
   homepage "https://github.com/GeneXisD/ritual"
   url "https://github.com/GeneXisD/ritual/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "3ee4c63164789dc35d389293e30b2bb0d2c236f5d910fb8eb909f58d5114a450"
+  sha256 "fcdffe341d504f103cd48575cdd2feeaab5e022cc0ab64147216b8d01dce7de2"
   license "MIT"
 
   depends_on "python@3.12"
