e4de17c3087749f42bdf4cd95eadfb4a6746a2e4
Victor J Corral <victorjcorral@icloud.com>
2025-07-25 21:40:06 -0700

    Update ritual.rb

--- Formula/ritual.rb
+++ Formula/ritual.rb
@@ -3,10 +3,10 @@ class Ritual < Formula
 
   desc "Portable CLI tool for cryptographic signing with PGPy"
   homepage "https://github.com/GeneXisD/ritual"
-  url "https://github.com/GeneXisD/ritual/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 ""
+  url "https://github.com/GeneXisD/ritual/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "db3b9fd959499f44d69a2218d3e246773ee39aef2795999d3221e8708a9d3680"
 "
-  version "0.1.1"
+  version "1.0.0"
   license "GPL-3.0-or-later"
 
   depends_on "python@3.12"
